991be04b5d4f4891677bc3b912583898436a81a0
yqchilde <yqchilde@gmail.com>
2023-02-27 20:52:41 +0800

    perf: 提交dist

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# 构建版本
 cd wxbot-web || exit
 version=$(git describe --abbrev=0 --tags)
 sed -i -e "s/0.0.0/${version:1}/g" package.json
 cat package.json
 
+# 打包
 npm install
 npm run build
 cp -r dist ../
 cd ../
 
+# 提交dist
+cd web-dist
+rm -rf dist
+cp -r ../dist .
+git add .
+git config --local user.name "yqchilde"
+git config --local user.email "yqchilde@gmail.com"
+git commit --allow-empty -m "upload ${version} dist files" -a
+git tag -a "$version" -m "release ${version}"
+cd ..
+
+# 压缩
 mkdir compress
 tar -czvf compress/wxbot-web.tar.gz -C dist/ .
 rm -rf dist
